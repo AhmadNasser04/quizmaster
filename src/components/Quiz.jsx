943ab27b838e6c57d98ef1312ee17d255280677c
Ahmad Nasser <ahmad_nasser04@outlook.com>
2022-11-29 20:40:28 +0200

    idk

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -21,15 +21,17 @@ function Quiz() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        if (checked === questions[current].correct) { setScore(score + 1) }
-        if (current < 9) { setCurrent(current + 1) } else { setDone(true) }
-        setChecked('none')
+        if (checked !== 'none') {
+            if (checked === questions[current].correct) { setScore(score + 1) }
+            if (current < 9) { setCurrent(current + 1) } else { setDone(true) }
+            setChecked('none')
+        }
     } 
 
   return (
     <div className='min-h-screen bg-[#131515]'>
         <div className='text-[#339989] text-center p-10 font-bold text-4xl justify-center flex'>{quiz === 'yahoo' && 'Yahoo! Data Breach Quiz'}</div>
-        <div className='border-t-2 m-10 border-[#339989] flex items-center justify-center' />
+        <div className='border-t-2 mb-10 mx-10 border-[#339989] flex items-center justify-center' />
         <div className=''> 
             {done ? <Statistic percentage={score * 10} text={`${score} / 10`}/> : <QuizContainer onClick={handleSubmit} current={current} questions={questions} checked={checked} onOptionChange={onOptionChange} /> }
             {done && 
