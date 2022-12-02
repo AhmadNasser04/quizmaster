314114b2d2b8887ed6e656d7bf05d5d3658f2ce3
Ahmad Nasser <106764307+AhmadNasser04@users.noreply.github.com>
2022-12-02 20:22:30 +0200

    Update questions.js

--- src/components/questions.js
+++ src/components/questions.js
@@ -1,60 +1,60 @@
 export const yahoo_questions = [
     {
-        "question": 'Question 1',
-        "options": ['Option 1', 'Option 2', 'Option 3'],
-        "correct": 'Option 3',
+        "question": 'When did the first breach happen?',
+        "options": ['2013', '2014', '2016'],
+        "correct": '2013',
         "index": 0
     },
     {
-        "question": 'Question 2',
-        "options": ['Option 1', 'Option 2', 'Option 3'],
-        "correct": 'Option 2',
+        "question": 'When did the second breach happen',
+        "options": ['2013', '2014', '2016'],
+        "correct": '2014',
         "index": 1
     },
     {
-        "question": 'Question 3',
-        "options": ['Option 1', 'Option 2', 'Option 3'],
-        "correct": 'Option 1',
+        "question": 'When was the discovery',
+        "options": ['2013', '2014', '2016'],
+        "correct": '2016',
         "index": 2
     },
     {
-        "question": 'Question 4',
-        "options": ['Option 1', 'Option 2', 'Option 3'],
-        "correct": 'Option 1',
+        "question": 'How many users were affected in the first breach',
+        "options": ['1 BIL', '200 MIL', '500 MIL'],
+        "correct": '1 BIL',
         "index": 3
     },
     {
-        "question": 'Question 5',
-        "options": ['Option 1', 'Option 2', 'Option 3'],
-        "correct": 'Option 3',
+        "question": 'How many accounts were being sold?',
+        "options": ['200 MIL', '500 MIL', '1 BIL'],
+        "correct": '200 MIL',
         "index": 4
     },
     {
-        "question": 'Question 6',
-        "options": ['Option 1', 'Option 2', 'Option 3'],
-        "correct": 'Option 2',
+        "question": 'How many people did the FBI charge?',
+        "options": ['3', '4', '5'],
+        "correct": '4',
         "index": 5
     },
     {
-        "question": 'Question 7',
-        "options": ['Option 1', 'Option 2', 'Option 3'],
-        "correct": 'Option 2',
+        "question": 'How did Yahoo find out how the hackers did it',
+        "options": ['Unknown', 'Spear-phishing', 'Fake cookies'],
+        "correct": 'Fake cookies',
         "index": 6
     },
     {
-        "question": 'Question 8',
-        "options": ['Option 1', 'Option 2', 'Option 3'],
-        "correct": 'Option 1',
+        "question": 'The name of the seller was:',
+        "options": ['Peace', 'Destruction', 'None of the above'],
+        "correct": 'Peace',
         "index": 7
     },
     {
-        "question": 'Question 9',
-        "options": ['Option 1', 'Option 2', 'Option 3'],
+        "question": 'What did one of the buyers Peace to check for',
+        "options": ['Names of 10 government officials', 'Names of 10 government eomployees', 'Names of 11 government officials'],
         "correct": 'Option 3',
         "index": 8
     },
     {
-        "question": 'Question 10',
+        "question": 'TRY YOUR LUCK - PICK RANDOMLY',
         "options": ['Option 1', 'Option 2', 'Option 3'],
         "correct": 'Option 2',
         "index": 9
